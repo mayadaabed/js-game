9162407e3afc017ad653bbb1a31bd445f3a8f2ed
Mayada Almobayed <abedmayada4@gmail.com>
2023-01-09 17:25:25 +0200

    Changes

--- projectJs.js
+++ projectJs.js
@@ -3,6 +3,8 @@ const palyerChocies = ["Rock", "Paper", "Scissors"];
 const palyerChociesImg = ["🪨", "📄", "✂️"];
 var playerChoice = "";
 var computerChoice = "";
+let pScore = 0;
+let cScore = 0;
 
 const buttonsContainer = document.getElementById("button-choice");
 
@@ -23,6 +25,11 @@ function events(button, i) {
         playerChoice = palyerChocies[i];
         computerChoices();
         decideTheWinner(computerChoice, playerChoice);
+        updateScore();
+        if (checkWinner()) {
+            pScore = cScore = 0;
+            updateScore();
+        }
     });
 }
 
@@ -30,37 +37,43 @@ function decideTheWinner(computer, player) {
     let result = document.getElementById("result");
 
     if (computer === player) {
-        result.innerText = null;
+        result.innerText = "Tie";
     }
     if (computer === "Rock" && player === "Scissors") {
         result.innerText = "YOU LOSE";
+        cScore++;
     }
 
     if (computer === "Scissors" && player === "Paper") {
         result.innerText = "YOU LOSE";
+        cScore++;
     }
 
     if (computer === "Paper" && player === "Rock") {
         result.innerText = "YOU LOSE";
+        cScore++;
     }
 
     if (computer === "Scissors" && player === "Rock") {
         result.innerText = "YOU Win";
+        pScore++;
     }
 
     if (computer === "Paper" && player === "Scissors") {
         result.innerText = "YOU Win";
+        pScore++;
     }
 
     if (computer === "Rock" && player === "Paper") {
         result.innerText = "YOU Win";
+        pScore++;
     }
 }
 
 function computerChoices() {
     const computerResult = document.getElementById("computerChoice");
     const computerResultImg = document.getElementById("computerChoiceImg");
-    const rand = Math.floor(Math.random()* palyerChocies.length);
+    const rand = Math.floor(Math.random() * palyerChocies.length);
     const ran = palyerChocies[rand];
     const ran1 = palyerChociesImg[rand];
     computerChoice = ran;
@@ -71,6 +84,39 @@ function computerChoices() {
 
 const palyAgain = document.getElementById("playAgain");
 
-palyAgain.addEventListener("click", function (){
+palyAgain.addEventListener("click", function () {
     window.location.reload();
 });
+
+function updateScore() {
+    document.getElementById("p-score").textContent = pScore;
+    document.getElementById("c-score").textContent = cScore;
+}
+
+function checkWinner() {
+    if (pScore === 5 || cScore === 5) {
+        const winner =
+            pScore === 5
+                ? "You win the game! Congratulations!"
+                : "You Lose";
+        const ale = document.getElementById("alert");
+        ale.style.display = "block";
+        ale.style.backgroundColor = pScore === 5
+            ? "green"
+            : "red";
+        ale.innerText = winner;
+        return true;
+    }
+    return false;
+}
+
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function(){
+    var div = this.parentElement;
+    div.style.opacity = "0";
+    setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
+}
